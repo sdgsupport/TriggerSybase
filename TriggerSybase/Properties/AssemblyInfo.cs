0cebb985962fdd1d88a2dd13a536a17d5bb9cb68
Angel <angelaragonf@gmail.com>
2025-06-24 16:55:26 -0600

    primer commit

---
+++ TriggerSybase/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("TriggerSybase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriggerSybase")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(true)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("5c40e0a6-28aa-45cc-a08f-6cae7596650a")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriggerSybase/trigger.cs
@@ -0,0 +1,88 @@
+﻿using Advantage.Data.Provider;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriggerSybase
+{
+    public class trigger
+    {   
+        public int triggerInsert(Int32 ulConnectionID, Int32 hConnection, String strTriggerName, String strTableName,
+                      Int32 ulEventType, Int32 ulTriggerType, Int32 ulRecNo)
+        { 
+            AdsConnection conn = new AdsConnection("ConnectionHandle="+hConnection.ToString());
+            AdsCommand cmd;
+            AdsCommand cmdNew;
+            AdsExtendedReader extReader;
+            AdsDataReader reader;
+            dynamic user = new ExpandoObject();
+            Guid guid = Guid.NewGuid();
+            string s = guid.ToString();
+            try
+            {
+                conn.Open();
+                cmd = conn.CreateCommand();
+                cmdNew = conn.CreateCommand();
+                
+
+                cmd.CommandText = "SELECT PkData, CardNumberFormatted, UserName, Email, Picture FROM __new";
+                reader = cmd.ExecuteReader();
+
+                //cmdNew.CommandText = "select*from ["+ strTableName + "]";
+                cmdNew.CommandText = "SELECT PkData, CardNumberFormatted, UserName, Email, Picture FROM log_Card";
+                extReader = cmdNew.ExecuteExtendedReader();
+
+                //insert __net
+                extReader.AppendRecord();
+                if (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        if (!reader.IsDBNull(i))
+                        {
+                            extReader.SetValue(i, reader.GetValue(i));
+                        }
+                    }
+                }
+                
+                extReader.WriteRecord();
+                extReader.Close();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                AdsCommand errCmd = conn.CreateCommand();
+                errCmd.CommandText = "INSERT INTO __error VALUES( 0, '" + ex.Message + "' )";
+                errCmd.ExecuteNonQuery();
+            }
+            return 0;
+        }
+
+        public int procedureInsertEvent(Int32 ulConnectionID, Int32 hConnection, String strTriggerName, String strTableName,
+                      Int32 ulEventType, Int32 ulTriggerType, Int32 ulRecNo)
+        {
+            AdsConnection conn = new AdsConnection("ConnectionHandle=" + hConnection.ToString());
+            AdsCommand cmd;
+            try
+            {
+                conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = "EXECUTE PROCEDURE sp_SignalEvent('evento_insert', false, 0, ::stmt.TrigRowID);";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) 
+            {
+                AdsCommand errCmd = conn.CreateCommand();
+                errCmd.CommandText = "INSERT INTO __error VALUES( 0, '" + ex.Message + "' )";
+                errCmd.ExecuteNonQuery();
+            }
+            return 0;
+        }
+    }
+}
